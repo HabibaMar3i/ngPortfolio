db1d6c65ee23b2d5d7b784817b6b14318cc2119b
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-13 21:47:25 +0300

    added project page

---
+++ src/app/core/models/project.interface.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    id?: number;
+    title?: string; 
+    description?: string;
+    tools?: string[];
+    features?: string[];
+    githubLink?: string;
+    liveLink?: string;
+    imageUrl?: string;
+}
--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Project } from '../../core/models/project.interface';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects: Project[] = [
+    {id: 1, title: 'GoCart', description: 'Ecommerce project', tools: ['Angular', 'Typescript', 'Tailwind-css'], features: ['feature a', 'feature b'], githubLink: 'www.google.com', liveLink: 'www.google.com', imageUrl: 'assets/images/GoCart.jpeg'},
+  ]
 }
