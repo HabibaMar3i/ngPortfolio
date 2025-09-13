d3373a20c4b92c5981b243ff93c41a60cc11e5f3
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-13 18:18:07 +0300

    added my projects section

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { AboutmeComponent } from "./aboutme/aboutme.component";
 import { MyskillsComponent } from "./myskills/myskills.component";
+import { MyprojectsComponent } from "./myprojects/myprojects.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, AboutmeComponent, MyskillsComponent],
+  imports: [AboutComponent, AboutmeComponent, MyskillsComponent, MyprojectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/features/home/myprojects/myprojects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-myprojects',
+  imports: [RouterLink],
+  templateUrl: './myprojects.component.html',
+  styleUrl: './myprojects.component.css'
+})
+export class MyprojectsComponent {
+
+}
