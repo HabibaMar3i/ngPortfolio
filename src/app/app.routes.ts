deb063f6dbcd2a89faeee78a6dfb23bd683f6d3d
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-21 22:31:50 +0300

    removed contact page files, routes and from navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,5 @@ export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent, title: 'Home'},
     {path: 'about', component: AboutComponent, title: 'About'},
-    {path: 'contact', component: ContactComponent, title: 'Contact'},
     {path: 'projects', component: ProjectsComponent, title: 'Projects'},
 ];
